65cc8a9e9eb14d485065d2e7ba927cd931ba9e5a
julian-vogels <julian.vogels@gmail.com>
2013-05-05 16:48:00 -0400

    Alpha Release
    Basic functionality all working and looking stable. To be used in
    conjunction with a RapsberryPi with an installed Libmapper distribution
    on a network or on the localhost with PureData.

--- gpio_encoder.py
+++ gpio_encoder.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 import time
 import os
+import atexit
 
 try:
     import RPi.GPIO as GPIO
@@ -10,241 +11,252 @@ except RuntimeError:
 import gaugette.rotary_encoder
 import gaugette.switch
 
+# Cleanup
+
+
+def all_done():
+    GPIO.cleanup()
+    print "GPIO Cleanup"
+    #lyra_mapper.dev.free()
+    #print "Libmapper cleanup"
+
+atexit.register(all_done)
+
 #LIBMAPPER IMPORT
-from lyra_mapper import LyraMapper
+import lyra_mapper
+#from lyra_mapper import LyraMapper
 
-lyra_mapper = LyraMapper()
+#lyra_mapper = LyraMapper()
 
-class LyraIO():
+#======================================
+#   GENERAL CONFIGURATION
+#======================================
+DEBUG = 1
 
-    def __init__(self):
+if DEBUG:
+    print "RaspberryPi Board Revision: ", GPIO.RPI_REVISION
+    print "GPIO version", GPIO.VERSION
+
+GPIO.setmode(GPIO.BOARD)
+
+sampling_frequency = 50.0
+sampling_period = 1.0/sampling_frequency
+
+#======================================
+#   PIN CONFIGURATION
+#======================================
+
+#============
+# ROTARY ENCODER PINS (BCM PIN NUMBERING!!!!!)
+#============
+A_PIN  = 7
+B_PIN  = 9
+# ENCODER PUSHBUTTON
+# ON MISO
+ENC_BUTTON = 21
+
+#============
+# MCP3008 ADC PINS
+#============
+# change these as desired - they're the pins connected from the
+# SPI port on the ADC to the Cobbler
+SPICLK = 12     #18 BCM
+SPIMISO = 16    #23
+SPIMOSI = 18    #24
+SPICS = 22      #25
+
+#============
+# SWITCH PINS
+#============
+# STATE ONE ON GPIO2 / R1R2
+SWITCH1 = 13
+# STATE TWO ON GPIO3
+SWITCH2 = 15
+
+#============
+# LED
+#============
+# ON GPIO17
+LED = 11
+
+#============
+# ARCADE BUTTON
+#============
+# ON MOSI
+BUTTON = 19
+
+#======================================
+#   DIGITAL READS CONFIGURATION
+#======================================
+GPIO.setup(SWITCH1, GPIO.IN)
+GPIO.setup(SWITCH2, GPIO.IN)
+GPIO.setup(LED, GPIO.OUT)
+GPIO.setup(BUTTON, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+GPIO.setup(ENC_BUTTON, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+
+# EVENT CALLBACK FUNCTIONS
+
+def button_callback():
+    print('Button press HIGH')
+    lyra_mapper.updateSignal(1, libm_button_val)
+
+def switch1_callback():
+    print('Switch 1 state change HIGH')
+    lyra_mapper.updateSignal(2, libm_switch1_val)
+
+def switch2_callback():
+    print('Switch 2 state change HIGH')
+    lyra_mapper.updateSignal(3, libm_switch2_val)
+
+def enc_button_callback():
+    print('Encoder button press HIGH')
+    lyra_mapper.updateSignal(5, libm_enc_button_val)
+
+def read_switch():
+    val1 = GPIO.input(SWITCH1)
+    val2 = GPIO.input(SWITCH2)
+    return val1;
+    # TODO edge detection
+
+def read_button():
+    button_state = GPIO.input(BUTTON)
+    return button_state
+
+def write_led(led_state):
+    if (led_state == True):
+        GPIO.output(LED, GPIO.HIGH)
+    else:
+        GPIO.output(LED, GPIO.LOW)
+
+GPIO.add_event_detect(BUTTON, GPIO.RISING, callback=button_callback)  
+GPIO.add_event_detect(SWITCH1, GPIO.RISING, callback=switch1_callback)  
+GPIO.add_event_detect(SWITCH2, GPIO.RISING, callback=switch2_callback)
+GPIO.add_event_detect(ENC_BUTTON, GPIO.RISING, callback=enc_button_callback)  
+
+#======================================
+#   ENCODER CONFIGURATION
+#======================================
+
+encoder = gaugette.rotary_encoder.RotaryEncoder.Worker(A_PIN, B_PIN)
+encoder.start()
+
+# read data from rotary encoder using gaugette lib
+def read_encoder():
+    return encoder.get_delta()
+
+#======================================
+#   MCP3008 ADC CONFIGURATION
+#======================================
+
+# read SPI data from MCP3008 chip, 8 possible adc's (0 thru 7)
+def read_adc(adcnum, clockpin, mosipin, misopin, cspin):
+        if ((adcnum > 7) or (adcnum < 0)):
+                return -1
+        GPIO.output(cspin, True)
+
+        GPIO.output(clockpin, False)  # start clock low
+        GPIO.output(cspin, False)     # bring CS low
+
+        commandout = adcnum
+        commandout |= 0x18  # start bit + single-ended bit
+        commandout <<= 3    # we only need to send 5 bits here
+        for i in range(5):
+                if (commandout & 0x80):
+                        GPIO.output(mosipin, True)
+                else:
+                        GPIO.output(mosipin, False)
+                commandout <<= 1
+                GPIO.output(clockpin, True)
+                GPIO.output(clockpin, False)
+
+        adcout = 0
+        # read in one empty bit, one null bit and 10 ADC bits
+        for i in range(12):
+                GPIO.output(clockpin, True)
+                GPIO.output(clockpin, False)
+                adcout <<= 1
+                if (GPIO.input(misopin)):
+                        adcout |= 0x1
+
+        GPIO.output(cspin, True)
+        
+        adcout >>= 1       # first bit is 'null' so drop it
+        return adcout
+
+# set up the SPI interface pins
+GPIO.setup(SPIMOSI, GPIO.OUT)
+GPIO.setup(SPIMISO, GPIO.IN)
+GPIO.setup(SPICLK, GPIO.OUT)
+GPIO.setup(SPICS, GPIO.OUT)
+
+# SHARP Infrared Sensor connected to adc #0
+sensor0_adc_pin = 0;
+
+adc_last_state = 0      # this keeps track of the last potentiometer value
+tolerance = 10           # to keep from being jittery only change
+                        # libm_sensor0_val when sensor has moved more than 5 'counts'
+                        # NOTE: NOT USED, will be handled in PureData
+
+# main loop for sensor measurement
+while True:
         #======================================
-        #   GENERAL CONFIGURATION
+        #   DIGITAL READING
         #======================================
-        DEBUG = 1
+
+        # ! EDGE DETECTION IS USED TO LOWER CPU USAGE AND AVOID MISSING STATE CHANGES
+
+        # libm_button_val = read_button()
+        # write_led(libm_button_val)
+        # if DEBUG:
+        #     print "Button State: ", libm_button_val
+
+        # libm_switch_val = read_switch()
+        # if DEBUG:
+        #     print "Switch State: ", libm_switch_val
+
+
+        #======================================
+        #   ROTARY ENCODER READING
+        #======================================
+        libm_encoder_val = read_encoder()
+        if libm_encoder_val!=0:
+            lyra_mapper.updateSignal(4, libm_encoder_val)
+        if DEBUG:
+            if libm_encoder_val!=0:
+                print "Encoder Direction: ", libm_encoder_val
+
+        #======================================
+        #   MCP3008 ADC READING
+        #======================================
+
+        # we'll assume that the pot didn't move
+        libm_sensor0_val_changed = False
+
+        # read the analog pin
+        libm_sensor0_val = read_adc(sensor0_adc_pin, SPICLK, SPIMOSI, SPIMISO, SPICS)
+        # how much has it changed since the last read?
+        sensor0_adjust = abs(libm_sensor0_val - adc_last_state)
 
         if DEBUG:
-            print "RaspberryPi Board Revision: ", GPIO.RPI_REVISION
-            print "GPIO version", GPIO.VERSION
+                pass
+                #print "libm_sensor0_val:", libm_sensor0_val
+                #print "sensor0_adjust:", sensor0_adjust
+                #print "adc_last_state", adc_last_state
 
-        GPIO.setmode(GPIO.BOARD)
+        if ( sensor0_adjust > tolerance ):
+               libm_sensor0_val_changed = True
 
-        sampling_frequency = 50.0
-        sampling_period = 1.0/sampling_frequency
+        if DEBUG:
+                pass
+                #print "libm_sensor0_val_changed", libm_sensor0_val_changed
 
-        #======================================
-        #   PIN CONFIGURATION
-        #======================================
+        if ( libm_sensor0_val_changed ):
+            lyra_mapper.updateSignal(6, libm_sensor0_val)
 
-        #============
-        # ROTARY ENCODER PINS (BCM PIN NUMBERING!!!!!)
-        #============
-        A_PIN  = 7
-        B_PIN  = 9
-        # ENCODER PUSHBUTTON
-        # ON MISO
-        ENC_BUTTON = 21
+        # save the sensor reading for the next loop
+        adc_last_state = libm_sensor0_val
+        # hang out and do nothing for the sampling period
+        # time.sleep(sampling_period)
 
-        #============
-        # MCP3008 ADC PINS
-        #============
-        # change these as desired - they're the pins connected from the
-        # SPI port on the ADC to the Cobbler
-        SPICLK = 12     #18 BCM
-        SPIMISO = 16    #23
-        SPIMOSI = 18    #24
-        SPICS = 22      #25
-
-        #============
-        # SWITCH PINS
-        #============
-        # STATE ONE ON GPIO2 / R1R2
-        SWITCH1 = 13
-        # STATE TWO ON GPIO3
-        SWITCH2 = 15
-
-        #============
-        # LED
-        #============
-        # ON GPIO17
-        LED = 11
-
-        #============
-        # ARCADE BUTTON
-        #============
-        # ON MOSI
-        BUTTON = 19
-
-        #======================================
-        #   DIGITAL READS CONFIGURATION
-        #======================================
-        GPIO.setup(SWITCH1, GPIO.IN)
-        GPIO.setup(SWITCH2, GPIO.IN)
-        GPIO.setup(LED, GPIO.OUT)
-        GPIO.setup(BUTTON, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-        GPIO.setup(ENC_BUTTON, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-
-        # EVENT CALLBACK FUNCTIONS
-
-        def button_callback():
-            print('Button press HIGH')
-
-        def switch1_callback():
-            print('Switch 1 state change HIGH')
-
-        def switch2_callback():
-            print('Switch 2 state change HIGH')
-
-        def enc_button_callback():
-            print('Encoder button press HIGH')
-
-        def read_switch():
-            val1 = GPIO.input(SWITCH1)
-            val2 = GPIO.input(SWITCH2)
-            return val1;
-            # TODO edge detection
-
-        def read_button():
-            button_state = GPIO.input(BUTTON)
-            return button_state
-
-        def write_led(led_state):
-            if (led_state == True):
-                GPIO.output(LED, GPIO.HIGH)
-            else:
-                GPIO.output(LED, GPIO.LOW)
-
-        GPIO.add_event_detect(BUTTON, GPIO.RISING, callback=button_callback)  
-        GPIO.add_event_detect(SWITCH1, GPIO.RISING, callback=switch1_callback)  
-        GPIO.add_event_detect(SWITCH2, GPIO.RISING, callback=switch2_callback)
-        GPIO.add_event_detect(ENC_BUTTON, GPIO.RISING, callback=enc_button_callback)  
-
-        #======================================
-        #   ENCODER CONFIGURATION
-        #======================================
-
-        encoder = gaugette.rotary_encoder.RotaryEncoder.Worker(A_PIN, B_PIN)
-        encoder.start()
-
-        # read data from rotary encoder using gaugette lib
-        def read_encoder():
-            return encoder.get_delta()
-
-        #======================================
-        #   MCP3008 ADC CONFIGURATION
-        #======================================
-
-        # read SPI data from MCP3008 chip, 8 possible adc's (0 thru 7)
-        def read_adc(adcnum, clockpin, mosipin, misopin, cspin):
-                if ((adcnum > 7) or (adcnum < 0)):
-                        return -1
-                GPIO.output(cspin, True)
-
-                GPIO.output(clockpin, False)  # start clock low
-                GPIO.output(cspin, False)     # bring CS low
-
-                commandout = adcnum
-                commandout |= 0x18  # start bit + single-ended bit
-                commandout <<= 3    # we only need to send 5 bits here
-                for i in range(5):
-                        if (commandout & 0x80):
-                                GPIO.output(mosipin, True)
-                        else:
-                                GPIO.output(mosipin, False)
-                        commandout <<= 1
-                        GPIO.output(clockpin, True)
-                        GPIO.output(clockpin, False)
-
-                adcout = 0
-                # read in one empty bit, one null bit and 10 ADC bits
-                for i in range(12):
-                        GPIO.output(clockpin, True)
-                        GPIO.output(clockpin, False)
-                        adcout <<= 1
-                        if (GPIO.input(misopin)):
-                                adcout |= 0x1
-
-                GPIO.output(cspin, True)
-                
-                adcout >>= 1       # first bit is 'null' so drop it
-                return adcout
-
-        # set up the SPI interface pins
-        GPIO.setup(SPIMOSI, GPIO.OUT)
-        GPIO.setup(SPIMISO, GPIO.IN)
-        GPIO.setup(SPICLK, GPIO.OUT)
-        GPIO.setup(SPICS, GPIO.OUT)
-
-        # SHARP Infrared Sensor connected to adc #0
-        sensor0_adc_pin = 0;
-
-        adc_last_state = 0      # this keeps track of the last potentiometer value
-        tolerance = 5           # to keep from being jittery only change
-                                # libm_sensor0_val when sensor has moved more than 5 'counts'
-                                # NOTE: NOT USED, will be handled in PureData
-
-        # main loop for sensor measurement
-        while True:
-                #======================================
-                #   DIGITAL READING
-                #======================================
-
-                # ! EDGE DETECTION IS USED TO LOWER CPU USAGE AND AVOID MISSING STATE CHANGES
-
-                # libm_button_val = read_button()
-                # write_led(libm_button_val)
-                # if DEBUG:
-                #     print "Button State: ", libm_button_val
-
-                # libm_switch_val = read_switch()
-                # if DEBUG:
-                #     print "Switch State: ", libm_switch_val
-
-
-                #======================================
-                #   ROTARY ENCODER READING
-                #======================================
-                libm_encoder_val = read_encoder()
-                if DEBUG:
-                    if libm_encoder_val!=0:
-                        print "Encoder Direction: ", libm_encoder_val
-
-                #======================================
-                #   MCP3008 ADC READING
-                #======================================
-
-                # we'll assume that the pot didn't move
-                analog_sensor0_changed = False
-
-                # read the analog pin
-                analog_sensor0 = read_adc(sensor0_adc_pin, SPICLK, SPIMOSI, SPIMISO, SPICS)
-                # how much has it changed since the last read?
-                sensor0_adjust = abs(analog_sensor0 - adc_last_state)
-
-                if DEBUG:
-                        pass
-                        #print "analog_sensor0:", analog_sensor0
-                        #print "sensor0_adjust:", sensor0_adjust
-                        #print "adc_last_state", adc_last_state
-
-                if ( sensor0_adjust > tolerance ):
-                       analog_sensor0_changed = True
-
-                if DEBUG:
-                        pass
-                        #print "analog_sensor0_changed", analog_sensor0_changed
-
-                if ( analog_sensor0_changed ):
-                        libm_sensor0_val = analog_sensor0 / 8.0           # convert 10bit adc0 (0-1024) sensor read into 0-127 MIDI std
-                        
-                        # DO STUFF
-
-
-                        if DEBUG:
-                                pass
-                                #print "Calculated value: ", libm_sensor0_val
-
-                # save the sensor reading for the next loop
-                adc_last_state = analog_sensor0
-                # hang out and do nothing for the sampling period
-                # time.sleep(sampling_period)
+        # poll the device
+        lyra_mapper.do_poll()
--- lyra_mapper.py
+++ lyra_mapper.py
@@ -1,59 +1,46 @@
 #!/usr/bin/env python
 
-import mapper
+import mapper, sys
 import math
 
+# function to receive a signal through libmapper
+def h(sig, id, f, timetag):
+	try:
+		print sig.name, f
+	except:
+		print 'exception'
+		print sig, f
 
-class LyraMapper():
-
-	# function to receive a signal through libmapper
-	def h(sig, id, f, timetag):
-    try:
-        print sig.name, f
-    except:
-        print 'exception'
-        print sig, f
-
-	# function called on initialization
-	def __init__(self):
-		s = Server().boot()
-		s.start()
-		try:
-			self.dev = mapper.device("Lyrabox", 9000)
-			self.sig1 = self.dev.add_output("/button", 1, 'i', "normalized", 0, 1)
-			self.sig2 = self.dev.add_output("/switch1", 1, 'i', "normalized", 0, 1)
-			self.sig3 = self.dev.add_output("/switch2", 1, 'i', "normalized", 0, 1)
-			self.sig4 = self.dev.add_output("/encoder", 1, 'i', "normalized", 0, 1)
-			self.sig5 = self.dev.add_output("/enc_button", 1, 'i', "normalized", 0, 1)
-			self.sig6 = self.dev.add_output("/analog_sensor0", 1, 'f', "normalized", 0, 1024)
-			self.sig7 = self.dev.add_input("/led", 1, 'i', "normalized", 0, 1, h)
-			while True:
-				dev.poll(0)
-		finally:
-			s.stop()
-
-	# function to update the libmapper signal
-	#	usage: signal number (int), sinal value (whatever is in range)
-	def updateSignal(signal, value):
-		if signal==1:
-			self.sig1.update(value)
-
-		if signal==2:
-			self.sig2.update(value)
-			
-		if signal==3:
-			self.sig3.update(value)
-			
-		if signal==4:
-			self.sig4.update(value)
-			
-		if signal==5:
-			self.sig5.update(value)
-			
-		if signal==6:
-			self.sig6.update(value)
+dev = mapper.device("Lyrabox", 9000)
+sig1 = dev.add_output("/button", 1, 'i', None, 0, 1)
+sig2 = dev.add_output("/switch1", 1, 'i', None, 0, 1)
+sig3 = dev.add_output("/switch2", 1, 'i', None, 0, 1)
+sig4 = dev.add_output("/encoder", 1, 'i', None, 0, 1)
+sig5 = dev.add_output("/enc_button", 1, 'i', None, 0, 1)
+sig6 = dev.add_output("/analog_sensor0", 1, 'f', None, 0, 1024)
+sig_in = dev.add_input("/led", 1, 'i', "normalized", 0, 1, h)
 
 
+# function to update the libmapper signal
+#	usage: signal number (int), sinal value (whatever is in range)
+def updateSignal(signal, value):
+	if signal==1:
+		sig1.update(value)
 
+	if signal==2:
+		sig2.update(value)
+		
+	if signal==3:
+		sig3.update(value)
+		
+	if signal==4:
+		sig4.update(value)
+		
+	if signal==5:
+		sig5.update(value)
+		
+	if signal==6:
+		sig6.update(value)
 
-	
+def do_poll():
+    dev.poll(0)
